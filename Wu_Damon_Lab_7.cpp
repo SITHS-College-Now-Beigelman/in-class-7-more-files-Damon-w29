777046a3ea8cdaab48a31e0e52636035ea2dc743
Damon-w29 <Damonw18@nycstudents.net>
2024-11-02 11:44:41 -0400

    fixes

--- Wu_Damon_Lab_7.cpp
+++ Wu_Damon_Lab_7.cpp
@@ -45,16 +45,16 @@ int main(){
     cout<< "processing informatiom \n";
 
     // does calculation of scores for player1 
-    infile >> Name;                                     // takes in the player name
-    outfile << "Player 1: " << Name << endl;            // shows the name
-    infile >> pl1score1 >> pl1score2 >> pl1score3;      // takes in scores
-    avg1 = ((pl1score1 + pl1score2 + pl1score3) /3);    // calculates average
+    infile >> Name;                                                          // takes in name
+    infile >> pl1score1 >> pl1score2 >> pl1score3;                           // takes in scores          
+    avg1 = ((pl1score1 + pl1score2 + pl1score3) /3);                         // calculates average
+    outfile << "Player 1: " << Name <<  " score: "<< avg1 << endl;            // shows the name
 
     // tells skill level depending on the score. 
         if (avg1 < 1000)
             outfile << Name << ", Keep Practicing! \n";
         if (avg1 >= 1000 && avg1 <= 1500)
-            outfile << Name << ", youre intermediate \n";
+            outfile << Name << ", You're an intermediate! \n";
         if (avg1 >= 1500 && avg1 <= 1800)
             outfile << Name << ", You're advanced, Great Job! \n";
         if (avg1 >= 1800 && avg1 <= 2000)
@@ -64,15 +64,15 @@ int main(){
     
     // does calculation of scores for player 2
     infile >> Name2;                                    // takes in the player name
-    outfile << "Player 2: " << Name2 << endl;           // shows the name
     infile >> pl2score1 >> pl2score2 >> pl2score3;      // takes in scores
     avg2 = ((pl2score1 + pl2score2 + pl2score3) /3);    // calculates average
+    outfile << "Player 2: " << Name2 << " score: "<< avg2 << endl;           // shows the name
 
     // tells skill level depending on the score. 
         if (avg2 < 1000)
             outfile << Name2 << ", Keep Practicing! \n";
         if (avg2 >= 1000 && avg2 <= 1500)
-            outfile << Name2 << ", youre intermediate \n";
+            outfile << Name2 << ", You're an intermediate! \n";
         if (avg2 >= 1500 && avg2 <= 1800)
             outfile << Name2 << ", You're advanced, Great Job! \n";
         if (avg2 >= 1800 && avg2 <= 2000)
@@ -82,15 +82,15 @@ int main(){
 
     // does calculation of scores for player 3
     infile >> Name3;                                    // takes in the player name
-    outfile << "Player 3: " << Name3 << endl;           // shows the name
     infile >> pl3score1 >> pl3score2 >> pl3score3;      // takes in scores
-    avg2 = ((pl3score1 + pl3score2 + pl3score3) /3);    // calculates average
+    avg3 = ((pl3score1 + pl3score2 + pl3score3) /3);    // calculates average
+    outfile << "Player 3: " << Name3 << " score: " << avg3 << endl;           // shows the name
 
     // tells skill level depending on the score. 
         if (avg3 < 1000)
             outfile << Name3 << ", Keep Practicing! \n";
         if (avg3 >= 1000 && avg3 <= 1500)
-            outfile << Name3 << ", youre intermediate \n";
+            outfile << Name3 << ", You're an intermediate! \n";
         if (avg3 >= 1500 && avg3 <= 1800)
             outfile << Name3 << ", You're advanced, Great Job! \n";
         if (avg3 >= 1800 && avg3 <= 2000)
